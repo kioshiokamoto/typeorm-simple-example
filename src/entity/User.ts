7b2f7f414ec45cd3481c85fdd0a77238bd53fcaa
Kioshi Okamoto <kjor29@gmail.com>
2021-04-10 20:14:56 -0500

    :dart: IMPROVE: agregar funciones crud con postgresql

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,5 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
-
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, CreateDateColumn, UpdateDateColumn, BeforeInsert } from 'typeorm';
+import {v4 as uuid} from 'uuid'
 @Entity("users")
 export class User extends BaseEntity{
 	@PrimaryGeneratedColumn()
@@ -13,4 +13,23 @@ export class User extends BaseEntity{
 
 	@Column()
 	role: string;
+
+	@Column({type:'uuid'})
+	uuid: string
+
+	@CreateDateColumn()
+	createdAt: Date
+
+	@UpdateDateColumn()
+	updatedAt: Date
+
+	@BeforeInsert()
+    createUuid() {
+        this.uuid = uuid()
+    }
+
+	toJSON(){
+		return { ...this, id:undefined}
+	}
+
 }
--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,68 @@ app.post('/users', async (req: Request, res: Response) => {
     }
 });
 //READ
+app.get('/users', async (_: Request, res: Response) => {
+    
+    try {
+        const users = await User.find();
+
+        return res.status(200).json(users)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:'Something went wrong'})
+    }
+});
 //UPDATE
+app.put('/users/:uuid', async (req: Request, res: Response)=>{
+    const uuid = req.params.uuid;
+    const {name, email, role} = req.body;
+
+    try {
+        const user = await User.findOneOrFail({uuid})
+        user.name = name || user.name;
+        user.email = email || user.email;
+        user.role = role || user.role;
+
+        await user.save();
+
+        return res.json(user);
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:'Something went wrong'})
+    }
+})
 //DELETE
+app.delete('/users/:uuid', async (req: Request, res: Response)=>{
+    const uuid = req.params.uuid;
+
+    try {
+        const user = await User.findOneOrFail({uuid})
+
+
+        await user.remove();
+
+        return res.status(204).json({message:'User deleted successfully'});
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:'Something went wrong'})
+    }
+})
 //FIND
+app.get('/users/:uuid', async (req: Request, res: Response)=>{
+    const uuid = req.params.uuid;
+
+    try {
+        const user = await User.findOneOrFail({uuid})
+
+        return res.json(user);
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:'User not found'})
+    }
+})
 
 createConnection()
 	.then(async () => {
